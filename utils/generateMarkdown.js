dd5890e6b12be45496fd46d23f2b6776156782e2
Antonio Guedes <gguedesantonio@gmail.com>
2020-07-20 13:54:22 +1000

    Markdown file license badge added.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,25 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
 
-  
-  return `# ${data.title}
+  let licenseType;
 
-`;
+  if (`${data.license}` === 'MIT License') {
+    licenseType = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  } else if (`${data.license}` === 'GNU Lesser General Public License v3.0') {
+    licenseType = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+  } else if (`${data.license}` === 'Mozilla Public License 2.0') {
+    licenseType = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+  } else if (`${data.license}` === 'GNU Affero General Public License v3.0') {
+    licenseType = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)'
+  } else if (`${data.license}` === 'The Unlicense') {
+    licenseType = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+  } else if (`${data.license}` === 'Apache License 2.0') {
+    licenseType = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  } else {
+    licenseType = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  
+  return `# ${data.title}`;
 }
 
 module.exports = generateMarkdown;
